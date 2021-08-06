af9d94c117b5da1dc7cb490229d94da33748e5f1
David <David>
2021-08-05 19:31:31 -0700

    s

--- bashScripts/compile.sh
+++ bashScripts/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm ../spdz/Programs/Source/audit.mpc
-cp audit/audit.mpc ../spdz/Programs/Source/audit.mpc
-./../spdz/compile.py -R 64 -Z 2 audit
+rm ../spdz/Programs/Source/audit_a.mpc
+cp audit/audit_a.mpc ../spdz/Programs/Source/audit_a.mpc
+./../spdz/compile.py -R 64 -Z 2 audit_a
