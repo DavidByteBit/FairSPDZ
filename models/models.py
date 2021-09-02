0de4900ebd0cf6a74261ca806b197a51a0a8329c
David <David>
2021-09-01 18:45:00 -0700

    s

--- models/models.py
+++ models/models.py
@@ -85,7 +85,7 @@ class logistic_regression(model):
 
         self.data = sfix.Matrix(total_amount_of_rows, cols)
 
-        for i in list(range(model_owner_id)) + list(range(model_owner_id + 1, parties)):
+        for i in range(parties - 1):
             print(each_parties_rows)
             self.data.assign(self.load_data(cols, each_parties_rows[i][0], each_parties_rows[i][1]))
 
