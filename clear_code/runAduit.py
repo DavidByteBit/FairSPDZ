3dee8bac4313de5915c2c3cc34edfc9b80a94161
David <David>
2021-09-02 19:23:19 -0700

    s

--- clear_code/runAduit.py
+++ clear_code/runAduit.py
@@ -116,7 +116,7 @@ def _compile_spdz(settings_map):
     # and direct it to the Compiler directory in the spdz directory (skips run.mpc)
     __populate_spdz_files(settings_map)
 
-    subprocess.check_call("./../spdz/compile.py {a}".format(a=c), shell=True)
+    subprocess.check_call("./../../spdz/compile.py {a}".format(a=c), shell=True)
 
 
 def __populate_spdz_files(settings_map):
