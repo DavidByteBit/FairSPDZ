0aad5a140c4d0651af2f94d44949f51e71d5ffde
David <David>
2021-09-02 18:47:43 -0700

    s

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import sys
 
 # TODO: This is problematic, what if someone renames their directory from FairSPDZ to something else?
 # Need to ad inits to files
-from clear_code import runAduit
+from .clear_code import runAduit
 
 setting_file_path = sys.argv[1]
 
