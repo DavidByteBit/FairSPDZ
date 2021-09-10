2f2691258458e36972beaa20c39e553bd774c000
David <David>
2021-09-09 19:42:46 -0700

    s

--- clear_code/networking/client.py
+++ clear_code/networking/client.py
@@ -14,22 +14,31 @@ def run(settings_map, metadata, host_ip=None, share_party_id=True, introduce=Fal
         print("Connecting to Alice...\n")
         time.sleep(1)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host_ip, host_port))
+    attempts = 0
+    max_attempts = 50
 
-        if introduce:
-            print("Connected - transferring public data\n")
-            time.sleep(1)
+    while attempts < max_attempts:
+        attempts += 1
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((host_ip, host_port))
 
-        msg = ""
+                if introduce:
+                    print("Connected - transferring public data\n")
+                    time.sleep(1)
 
-        if share_party_id:
-            msg = str(party_id) + metadata
-        else:
-            msg = metadata
+                msg = ""
 
-        print("sending: " + msg)
-        s.sendall(str.encode(msg))
-        # data = s.recv(1024)
+                if share_party_id:
+                    msg = str(party_id) + metadata
+                else:
+                    msg = metadata
+
+                print("sending: " + msg)
+                s.sendall(str.encode(msg))
+                # data = s.recv(1024)
+
+        except Exception:
+            time.sleep(0.1)
 
     # print('Received', repr(data))
