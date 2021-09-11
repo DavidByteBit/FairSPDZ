8354358b453ae66dd791e66da2d3a57e302b8168
David <David>
2021-09-11 15:06:09 -0700

    s

--- clear_code/runAduit.py
+++ clear_code/runAduit.py
@@ -105,6 +105,7 @@ def _edit_source_code(settings_map, all_metadata):
 def __format_args(**kwargs):
     res = "{"
     for key in kwargs:
+        print(kwargs[key])
         if kwargs[key] is list:
             res += "\'{key}\': {value},".format(key=key, value=kwargs[key])
         else:
