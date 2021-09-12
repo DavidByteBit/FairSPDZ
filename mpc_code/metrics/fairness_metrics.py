654031943b0c2c9f87e9a3c33e106a0a440cb5e8
David <David>
2021-09-11 17:46:33 -0700

    s

--- mpc_code/metrics/fairness_metrics.py
+++ mpc_code/metrics/fairness_metrics.py
@@ -57,7 +57,7 @@ class metric():
             female[i] = sfix(0)
 
         print_ln("%s", protected_col.reveal())
-        print_ln(protected_col.length())
+        print_ln(len(protected_col))
 
         @for_range_opt(l)
         def _(i):
